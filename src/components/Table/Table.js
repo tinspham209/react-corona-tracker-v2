893dabfd484a36d8e3bb753e4a208fd07270bb57
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-04 23:03:50 +0700

    Refactor table with material-ui

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,31 +1,75 @@
 import React from "react";
 import CountUp from "react-countup";
 import Spinner from "../UI/Spinner/Spinner";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core/";
 import "./Table.css";
 
-const Table = (props) => {
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const TableLiveCases = (props) => {
   const { countries } = props;
-  let tableContent;
-  if (countries === undefined) {
-    tableContent = <Spinner />;
-  } else {
+
+  let tableContent = <Spinner />;
+  if (countries) {
     tableContent = (
-      <React.Fragment>
-        {countries.map(({ country, cases, countryInfo }) => (
-          <tr>
-            <td>{country}</td>
-            <td>
-              <strong>
-                <CountUp start={0} end={cases} duration={1.5} separator="," />
-              </strong>
-            </td>
-          </tr>
-        ))}
-      </React.Fragment>
+      <TableContainer className="table" component={Paper}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Country</StyledTableCell>
+              <StyledTableCell align="right">Cases</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {countries.map(({ country, cases }) => (
+              <StyledTableRow key={country}>
+                <StyledTableCell component="th" scope="row">
+                  {country}
+                </StyledTableCell>
+                <StyledTableCell align="right">
+                  {
+                    <CountUp
+                      start={0}
+                      end={cases}
+                      duration={1.5}
+                      separator=","
+                    />
+                  }
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     );
   }
   console.log("countries", countries);
-  return <div className="table">{tableContent}</div>;
+  return <React.Fragment>{tableContent}</React.Fragment>;
 };
 
-export default Table;
+export default TableLiveCases;
