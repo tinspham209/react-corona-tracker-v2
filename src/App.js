0d469d76f6763e03c22c6e5b6ce5692cdd540b08
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-04 15:52:51 +0700

    app__header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import { getCountriesData } from "./api";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
 
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setCountries(await getCountriesData());
+    };
+
+    fetchAPI();
+  }, [setCountries]);
+  console.log("countries", countries);
+
+  const onCountryChange = (event) => {
+    setCountry(event.target.value);
+  };
+
+  return (
+    <div className="app">
       {/* Header */}
       {/* Title + Select input dropdown field */}
+
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem key="worldwide" value="worldwide">
+              Worldwide
+            </MenuItem>
+            {countries.map((country) => (
+              <MenuItem key={country.value} value={country.value}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
       {/* InfoBox */}
       {/* InfoBox */}
