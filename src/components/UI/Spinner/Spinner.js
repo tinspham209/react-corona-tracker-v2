18612d65798104c87ec22586b7b15d62d5e6f591
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-04 18:50:42 +0700

    update Spinner with MaterialUI

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import styles from "./Spinner.css";
-const Spinner = () => {
-     return(
-          <div className={styles.Loader}>Loading...</div>
-     )
-}
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-export default Spinner
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > * + *": {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
+export default function CircularIndeterminate() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CircularProgress />
+    </div>
+  );
+}
