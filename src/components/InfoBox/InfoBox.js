557361bca4e8c231fd6aaa2e76b31a294d31b37c
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-04 18:01:52 +0700

    update UI InfoBox

--- src/components/InfoBox/InfoBox.js
+++ src/components/InfoBox/InfoBox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
+import CountUp from "react-countup";
 
 const InfoBox = (props) => {
   const { title, cases, total } = props;
@@ -9,9 +10,21 @@ const InfoBox = (props) => {
         <Typography className="infoBox__title" color="textSecondary">
           {title}
         </Typography>
-        <h2 className="infoBox__cases">{cases}</h2>
-        <Typography className="infoBox__total" color="textSecondary">
-          {total} Total
+        <Typography
+          className="infoBox__total"
+          variant="h5"
+          color="textSecondary"
+        >
+          {total}
+          {/* <CountUp start={0} end={total} duration={1.0} separator="," /> */}
+        </Typography>
+        <Typography
+          className="infoBox__total"
+          variant="h6"
+          color="textSecondary"
+        >
+          + {cases}
+          {/* <CountUp start={0} end={cases} duration={1.0} separator="," /> */}
         </Typography>
       </CardContent>
     </Card>
