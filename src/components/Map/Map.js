5fc09cdfc5043c5e624eb17322dc4a08e2e0ff5d
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-05 18:37:58 +0700

    Map component with Leaflet

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+import "./Map.css";
 
 const Map = (props) => {
-  return <div>I am Map</div>;
+  const { center, zoom } = props;
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </LeafletMap>
+    </div>
+  );
 };
 
 export default Map;
